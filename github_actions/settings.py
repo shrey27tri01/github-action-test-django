24b27867a89ba464c3dfc5dd90268f000c0c865d
shrey27tri01 <shrey.tripathi@iiitb.org>
2020-09-16 16:51:12 +0530

    Made postgres as default in settings.py

--- github_actions/settings.py
+++ github_actions/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'github_actions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'github_actions',
+        'USER': 'radorado',
+        'PASSWORD': 'radorado',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
